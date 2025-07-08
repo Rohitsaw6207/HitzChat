0643bafe5c2c9aae147ae2fa8916af86e7dc78a5
Rohitsaw6207 <rohitku6207@gmail.com>
2025-07-08 21:28:12 +0530

    fix: removed frontend static serving for Render

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,8 +3,6 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
-import path from "path";
-
 import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
@@ -14,10 +12,10 @@ import { app, server } from "./lib/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
@@ -28,13 +26,7 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+// ❌ Removed serving frontend build folder from backend (not needed on Render)
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
