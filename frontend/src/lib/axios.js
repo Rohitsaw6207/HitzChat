103ccba1c7658d1df7673ec6676a32caa4661ca5
Rohitsaw6207 <rohitku6207@gmail.com>
2025-07-08 21:33:34 +0530

    Updated axios baseURL for production

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? import.meta.env.VITE_API_BASE_URL
+      : "https://hitchat-backend.onrender.com/api",
   withCredentials: true,
 });
