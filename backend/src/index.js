fd1e0d53beba2025c6efb81a5a7761000c85fd3c
Rohitsaw6207 <rohitku6207@gmail.com>
2025-07-09 07:08:44 +0530

    fix: enable cross-site cookies for production

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import path from "path";
+
 import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
@@ -12,10 +14,10 @@ import { app, server } from "./lib/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
@@ -26,7 +28,13 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-// ❌ Removed serving frontend build folder from backend (not needed on Render)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
