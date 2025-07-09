ce744898fc4384e207dfbb422a41ae45bed67415
Rohitsaw6207 <rohitku6207@gmail.com>
2025-07-09 07:35:04 +0530

    Roll back

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,33 +2,32 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+
 import path from "path";
 
 import { connectDB } from "./lib/db.js";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
-const PORT = process.env.PORT || 5000;
+
+const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-// Middleware
 app.use(express.json());
 app.use(cookieParser());
-
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
 
-// API Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-// Serve frontend (render will auto-detect it)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
@@ -38,6 +37,6 @@ if (process.env.NODE_ENV === "production") {
 }
 
 server.listen(PORT, () => {
-  console.log("Server running on port", PORT);
+  console.log("server is running on PORT:" + PORT);
   connectDB();
 });
